8a8241803c1b65ead7049f18195c2c3b570ff773
josevalerio2703 <josevalerio2703@gmail.com>
2023-07-11 12:04:38 -0300

    Is Aactive

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,8 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useLocation } from "react-router-dom";
 
 const Layout = () => {
+  const location = useLocation();
+
   return (
     <div className="md:flex md:min-h-screen">
       <aside className="md:w-1/4 bg-blue-900 px-5 py-10">
@@ -9,8 +11,24 @@ const Layout = () => {
         </h2>
 
         <nav className="mt-10">
-          <Link className="text-2xl block text-white mt-2 hover:text-blue-300" to="/">Clientes</Link>
-          <Link className="text-2xl block text-white mt-2 hover:text-blue-300" to="/clientes/nuevo">Nuevo Cliente</Link>
+          <Link
+            className={`${
+              location.pathname === "/" ? "text-blue-300" : "text-white"
+            } text-2xl block mt-2 hover:text-blue-300`}
+            to="/"
+          >
+            Clientes
+          </Link>
+          <Link
+            className={`${
+              location.pathname === "/clientes/nuevo"
+                ? "text-blue-300"
+                : "text-white"
+            } text-2xl block mt-2 hover:text-blue-300`}
+            to="/clientes/nuevo"
+          >
+            Nuevo Cliente
+          </Link>
         </nav>
       </aside>
 
