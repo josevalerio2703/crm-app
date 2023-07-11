6000bab0193d98f2fbaffd29aa9687d699f98c91
josevalerio2703 <josevalerio2703@gmail.com>
2023-07-11 18:17:05 -0300

    Update NuevoCliente.jsx

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const NuevoCliente = () => {
   return (
-    <div>NuevoCliente</div>
-  )
-}
+    <>
+      <h1 className="font-black text-4xl text-blue-900">Nuevo Cliente</h1>
+      <p className="mt-3">Rellene todos los campos para crear un nuevo cliente</p>
+    </>
+  );
+};
 
-export default NuevoCliente 
+export default NuevoCliente;
