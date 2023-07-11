dacc3b45c822c207315fc3f0e82f10c2881fe884
josevalerio2703 <josevalerio2703@gmail.com>
2023-07-11 19:20:02 -0300

    Creando action a formulario

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,29 +1,39 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Form } from "react-router-dom";
 import Formulario from "../components/Formulario";
 
+export function action() {
+  console.log("Submit al Formulario......");
+ 
+}
 
 const NuevoCliente = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
     <>
       <h1 className="font-black text-4xl text-blue-900">Nuevo Cliente</h1>
-      <p className="mt-3">Rellene todos los campos para crear un nuevo cliente</p>
+      <p className="mt-3">
+        Rellene todos los campos para crear un nuevo cliente
+      </p>
 
       <div className="flex justify-end">
-        <button className="bg-blue-800 text-white px-3 font-bold uppercase "
-        onClick={()=> navigate(-1)}
-        >Volver</button>
+        <button
+          className="bg-blue-800 text-white px-3 font-bold uppercase "
+          onClick={() => navigate(-1)}
+        >
+          Volver
+        </button>
       </div>
 
       <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20">
-        <form >
-        <Formulario/>
+        <Form method="post" >
+          <Formulario />
 
-        <input type="submit" 
-        className="mt-5 w-full bg-blue-800 p-3 text-white uppercase font-bold tex-lg "
-        value='Registar Cliente'
-        />
-        </form>
+          <input
+            type="submit"
+            className="mt-5 w-full bg-blue-800 p-3 text-white uppercase font-bold tex-lg "
+            value="Registar Cliente"
+          />
+        </Form>
       </div>
     </>
   );
