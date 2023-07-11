b42f6217ddf746be40441a04a63b0d1be819cc40
josevalerio2703 <josevalerio2703@gmail.com>
2023-07-11 11:30:41 -0300

    Agregando componente Link

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,21 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
 const Layout = () => {
   return (
     <div className="md:flex md:min-h-screen">
       <aside className="md:w-1/4 bg-blue-900 px-5 py-10">
-        <h2 className="text-4xl font-black text-center text-white">CRM - Clientes</h2>
+        <h2 className="text-4xl font-black text-center text-white">
+          CRM - Clientes
+        </h2>
+
+        <nav className="mt-10">
+          <Link className="text-2xl block text-white mt-2 hover:text-blue-300" to="/">Clientes</Link>
+          <Link className="text-2xl block text-white mt-2 hover:text-blue-300" to="/clientes/nuevo">Nuevo Cliente</Link>
+        </nav>
       </aside>
 
       <main className="md:w-3/4 p-10 md:h-screen overflow-scroll">
-        <Outlet/>
+        <Outlet />
       </main>
     </div>
   );
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,6 +1,6 @@
 function Index() {
   return (
-    <div>Index</div>
+    <div>Clientes</div>
   )
 }
 
