684780fd6cd1787bda41cdde7b23908ebdca733e
josevalerio2703 <josevalerio2703@gmail.com>
2023-07-11 22:20:32 -0300

    Creando pantalla de error

--- src/data/clientes.jsx
+++ src/data/clientes.jsx
@@ -1,6 +1,5 @@
 export async function obtenerCliente(){
-const url = 'http://localhost:3000/clientes'
-
+    
 const respuesta = await fetch(import.meta.env.VITE_API_URL)
 const resultado = await respuesta.json()
 return resultado
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './components/Layout'
 import NuevoCliente, { action as NuevoClienteAction } from './pages/NuevoCliente'
 import Index, { loader as clientesLoader } from './pages/Index'
+import ErrorPage from './components/ErrorPage'
 
 
 
@@ -17,7 +18,8 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Index/>,
-        loader: clientesLoader
+        loader: clientesLoader,
+        errorElement: <ErrorPage/>
       },
       {
         path:'/clientes/nuevo',
